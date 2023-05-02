c6ba3a8460fc09f6d0edb15030ffe67f119b7236
shin.jewoo <shinjw0926@naver.com>
2023-05-02 22:21:57 +0900

    JDBC 개발 - 조회

--- src/main/java/study/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/study/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import study.jdbc.connection.DBConccectionUtil;
 import study.jdbc.domain.Member;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -12,7 +13,7 @@ import java.sql.*;
 @Slf4j
 public class MemberRepositoryV0 {
 
-    public Member save(Member member) throws SQLException {
+    public Member save(Member member) {
         String sql = "insert into member(member_id, money) values (?, ?)";
 
         try (
@@ -25,7 +26,33 @@ public class MemberRepositoryV0 {
         } catch (SQLException e) {
             log.error("error", e);
         }
-        return member;
+        return null;
+    }
+
+    public Member findById(final String memberId) {
+        String sql = "select * from member where member_id = ?";
+
+        try (
+                Connection conn = getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+        ) {
+            pstmt.setString(1, memberId);
+            try (ResultSet rs = pstmt.executeQuery();) {
+                if (rs.next()) {
+                    Member member = new Member();
+                    member.setMemberId(rs.getString("member_id"));
+                    member.setMoney(rs.getInt("money"));
+                    return member;
+                } else {
+                    throw new NoSuchElementException("member not found memberId = " + memberId);
+                }
+            } catch (SQLException e) {
+                log.error("error", e);
+            }
+        } catch (SQLException e) {
+            log.error("error", e);
+        }
+        return null;
     }
 
     private Connection getConnection() {
--- src/test/java/study/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/study/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,28 @@
 package study.jdbc.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import study.jdbc.domain.Member;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+
+        //save
+        Member member = new Member("memberV02", 10000);
         Member save = repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
